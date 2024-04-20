c8c2ec5f5c6366af797e446c144a7ee98e2363b7
emuzex <futomirko@gmail.com>
2024-04-20 10:36:54 +0200

    refactor: start refactor base-input styling

--- packages/virgo-vue/src/components/button/config.ts
+++ packages/virgo-vue/src/components/button/config.ts
@@ -2,16 +2,12 @@ import type { virgoButtonProps } from '@/components/button/meta'
 import type { ComponentClasses } from '@/plugin'
 
 export const virgoButtonClasses: ComponentClasses<virgoButtonProps> = {
-		button: ({ iconOnly, disabled, variant }) => {
+		button: ({ iconOnly, disabled }) => {
 			return [
 				'inline-flex whitespace-nowrap justify-center items-center relative text-white bg-purple-500 shadow-md shadow-purple-500/20 transition-all hover:shadow-lg hover:shadow-purple-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none',
 				iconOnly ? 'px-2 font-medium rounded-lg aspect-square min-w-10 focus-visible:ring-2 ring-offset-2 uno-layer-base-i:[&_.virgo-buttonn-content]-text-lg' : 'px-6 font-medium rounded-lg h-10 focus-visible:ring-2 ring-offset-2',
 				{
 					'opacity-50 pointer-events-none shadow-none': disabled,
-					'its-block': variant?.block,
-					'large-size': variant?.large,
-					'its-outline': variant?.outline,
-					'its-primary': variant?.primary,
 				}
 			]
 		},
@@ -29,7 +25,6 @@ export const virgoButtonClasses: ComponentClasses<virgoButtonProps> = {
 				}
 			]
 		},
-		something: 'asdasd'
 }
 
 export type virgoButtonClassesValidKeys = keyof typeof virgoButtonClasses
